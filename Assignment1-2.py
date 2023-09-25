4e0789c3515f6fbc653f0ff1f9766717bb3e65c4
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2023-09-25 14:42:28 +0530

    Add files via upload

---
+++ Assignment1-2.py
@@ -0,0 +1,10 @@
+string=str(input("enter word that need to be reversed"))
+revstring=""
+for i in reversed(string):
+    revstring+=i
+print(revstring)
+"""
+expected output
+enter word that need to be reversed edyoda
+adoyde
+"""
